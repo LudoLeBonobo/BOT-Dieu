f70dd43b408ec3e438010189e394490c39f83b5d
Ludovic Régnier <ludovic.regnier.2004@gmail.com>
2020-03-30 18:18:57 +0200

    Ajout d'options pour le client + optimisation

---
+++ main.js
@@ -0,0 +1,15 @@
+const { Client } = require('discord.js');
+const client = new Client({ disableEveryone: true });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') msg.channel.send('pong');
+  if (msg.content === 'everyone') msg.channel.send('@everyone, Salut à tous ^^', {disableEveryone: false});
+  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), Salut à tous ^^');
+});
+
+client.login('NjkyMDU0NzQxNjkzNDk3NDI0.XoHgwQ.xrVb0zDdliKXIWL4LbEnVuXuuyU');
+
